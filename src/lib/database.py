71e65ecfcb82ee56bd61a01a648e80bd4ab88c7b
cameron <camnooten@gmail.com>
2021-09-22 18:34:13 -0500

    update typing for create_database

--- src/lib/database.py
+++ src/lib/database.py
@@ -1,6 +1,6 @@
 from databases import Database
 
-async def create_database(db_uri: Database):
+async def create_database(db_uri: str) -> Database:
     database = Database(db_uri)
     await database.connect()
     return database
