bbaf733ba847356f5c5d6b59a6aee6054ab603c4
cameron <camnooten@gmail.com>
2021-09-24 16:36:43 -0500

    remove word

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,5 +7,6 @@ import pytest
 @pytest.fixture(name="app", scope="function")
 async def _app() -> AsyncGenerator[Quart, None]:
     app = create_app()
+    
     async with app.test_app():
         yield app
