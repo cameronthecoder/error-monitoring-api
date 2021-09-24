6582ee53707fc55e5c2f51830f91834e6cd2b284
cameron <camnooten@gmail.com>
2021-09-24 16:44:39 -0500

    fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+from src.lib.database import create_database
 from typing import AsyncGenerator
 from quart import Quart
 from src import create_app
@@ -7,6 +8,10 @@ import pytest
 @pytest.fixture(name="app", scope="function")
 async def _app() -> AsyncGenerator[Quart, None]:
     app = create_app()
-    
+    db = await create_database(app.config["DATABASE_URI"])
+    with app.open_resource("schema.sql", "r") as file_:
+        for command in file_.read().split(";"):
+            await db.execute(command)
+
     async with app.test_app():
         yield app
